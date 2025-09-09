7f893850d4204eca88d958aa389d251a2fd10d59
Jphan1418 <Josephaphan14@outlook.com>
2025-09-09 13:02:11 -0500

    BAH CAM

--- NekoAdventure/Assets/Scripts/CameraFollow.cs
+++ NekoAdventure/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ public class CameraFollow : MonoBehaviour
 {
     Transform playerTransform;
         	
-    public Vector3 cameraOrientationVector = new Vector3 (0, 15, -20f);
+    public Vector3 cameraOrientationVector = new Vector3 (0, 2.5f, -4.5f);
         	
 
     void Start () 
